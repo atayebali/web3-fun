bed0c0011050b991f7a0a59d70fb745cd4371d4a
Anis <atayebali@gmail.com>
2022-01-11 22:16:35 +0000

    Moving ether between accounts locally

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 var Web3 = require('web3')
-
 var web3 = new Web3("HTTP://127.0.0.1:8545")
 
-web3.eth.getAccounts().then(accounts => console.log(accounts))
+async function main() {
+  var receiving = "hash"
+  var rawtx = {
+    nonce: 7,
+    to: receiving,
+    gasPrice: 20000000000,
+    gasLimit: 300000,
+    value: 1,
+    data: ""
+  }
+
+  try {
+    const signedTx = await web3.eth.accounts.signTransaction(rawtx, 'pkey')
+    const tx = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+    console.log(tx)
+
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+
+main()
+
